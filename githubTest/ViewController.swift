4d8f590ed514228d2b44852acd2ed8a1205361df
Michael Lekrans <mlekrans@hotmail.com>
2019-07-08 15:30:04 +0200

    Initial Commit

---
+++ githubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubTest
+//
+//  Created by Michael Lekrans on 2019-07-08.
+//  Copyright © 2019 Michael Lekrans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
